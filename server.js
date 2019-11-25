75a970da515a9fe954a2794e0a7da921c3307fdd
Victor Hermes <me.victorhermes@gmail.com>
2019-11-25 15:30:20 -0300

    server

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
 const app = express();
+const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(router);
 
 app.get("/", function(req, res) {
   res.send("Hello World!");
